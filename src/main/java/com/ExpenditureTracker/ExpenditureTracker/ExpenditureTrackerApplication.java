0578c1e6b0780e39893b4497ee234f1ccee32d54
manuel-buser <buser.manuel@hotmail.com>
2025-03-24 20:15:20 +0100

    created new project

---
+++ src/main/java/com/ExpenditureTracker/ExpenditureTracker/ExpenditureTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ExpenditureTracker.ExpenditureTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenditureTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenditureTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExpenditureTracker/ExpenditureTracker/ExpenditureTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExpenditureTracker.ExpenditureTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenditureTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
